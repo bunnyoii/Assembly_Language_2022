811eaa177171e74e8b25f3ca27ae17f9bee26788
bunnyoii <Liushuyi1127@outlook.com>
2024-09-25 20:42:51 +0800

    +

---
+++ Assignment2_ASCII/IFELSE.asm
@@ -0,0 +1,46 @@
+.MODEL SMALL
+.STACK 100h
+.DATA
+    letters DB 'abcdefghijklmnopqrstuvwxyz$'  
+    newline DB 0Dh, 0Ah, '$'                  
+
+.CODE
+MAIN PROC
+    MOV AX, @DATA
+    MOV DS, AX
+
+    MOV CX, 26                                
+    LEA SI, letters                           
+
+    MOV BX, 13                                
+
+print_letters_conditional:
+    MOV AH, 02h                               
+    MOV DL, [SI]                              
+    INT 21h                                   
+
+    INC SI                                    
+    DEC CX                                    
+    JZ end_loop                               
+
+    DEC BX                                    
+    CMP BX, 0                                 
+    JNE print_letters_conditional             
+
+    MOV AH, 09h                               
+    LEA DX, newline                           
+    INT 21h                                   
+    MOV BX, 13                                
+
+    JMP print_letters_conditional             
+
+end_loop:
+    MOV AH, 09h
+    LEA DX, newline
+    INT 21h
+
+    MOV AH, 4Ch                               
+    INT 21h
+
+MAIN ENDP
+END MAIN
--- Assignment2_ASCII/loop.asm
+++ Assignment2_ASCII/loop.asm
@@ -1,31 +1,38 @@
 .MODEL SMALL
 .STACK 100h
 .DATA
-    letters DB 'abcdefghijklmnopqrstuvwxyz$'  
-    newline DB 0Dh, 0Ah, '$'                  
+    letters DB 'abcdefghijklmnopqrstuvwxyz$'
+    newline DB 0Dh, 0Ah, '$'
 
 .CODE
 MAIN PROC
     MOV AX, @DATA
     MOV DS, AX
 
-    MOV CX, 26                                
-    LEA SI, letters                           
+    MOV CX, 26
+    LEA SI, letters
+
+outer_loop:
+    PUSH CX
+    MOV CX, 13
 
 print_letters_loop:
-    MOV AH, 02h                               
-    MOV DL, [SI]                              
-    INT 21h                                   
+    MOV AH, 02h
+    MOV DL, [SI]
+    INT 21h
+    INC SI
+    LOOP print_letters_loop
 
-    INC SI                                    
-    LOOP print_letters_loop                   
+    MOV AH, 09h
+    LEA DX, newline
+    INT 21h
 
-    MOV AH, 09h                               
-    LEA DX, newline                           
-    INT 21h                                   
+    POP CX
+    SUB CX, 13
+    JNZ outer_loop
 
-    MOV AH, 4Ch                              
-    INT 21h                                   
+    MOV AH, 4Ch
+    INT 21h
 
 MAIN ENDP
 END MAIN
